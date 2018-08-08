605e541d48922e82388516a5b649ad0562943401
Fengmaybe <lyuya314@gmail.com>
2018-08-08 17:30:41 +0800

    login

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,14 +5,64 @@ Page({
    * 页面的初始数据
    */
   data: {
-  userName:'lyuya'
+  userName:'lyuya',
+  userInfo:{},
+  isShow:false, //是否显示button组件
   },
-
+  //对象的简写
+  goStart () {
+    console.log('我点击了开启小程序之旅');
+    // 点击跳转到list页面 保留当前页面，跳转到应用内的某个页面
+    wx.navigateTo({
+      //注意从根目录下查找
+      url: '/pages/list/list'
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-  
+  onLoad: function (options) { //初始化发送请求以及开启定时器
+    this.getUserInfo();
+  },
+  getUserInfo () {
+     //第一次授权
+    wx.getSetting({
+      success: (data) => {
+        console.log(data);
+        if (data.authSetting["scope.userInfo"]){
+          //授权成功
+          this.setData({
+            isShow:false
+          });
+        }else {
+          //授权失败
+          this.setData({
+            isShow: true
+          });
+        }
+      }
+    }),
+      //获取用户信息
+      wx.getUserInfo({
+        success: (data) => {
+          this.setData({
+            userInfo: data.userInfo,
+          });
+        },
+        fail: () => {
+          console.log("获取用户信息失败");
+        }
+      });
+  },
+
+  //wxml中button的自定义回调监听函数
+  handleGetUserInfo(data){
+      if (data.detail.rawData){
+        //用户点击了允许
+        this.getUserInfo();
+      }else{
+        console.log('failllll');
+      }
   },
 
   /**
