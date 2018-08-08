99c3bd9c9f2c6d840e6504bb3a4a64fc881bc3c7
Fengmaybe <lyuya314@gmail.com>
2018-08-08 19:21:47 +0800

    'list'

---
+++ datas/list-data.js
@@ -0,0 +1,151 @@
+let list_data = [
+  
+  {
+
+    date: 'may 19 2018',
+    title: '网易云音乐',
+    detail_img: '/images/detail/carousel/02.jpg',
+    avatar: '/images/avatar/4.png',
+    detail_content: '网易云音乐是一款专注于发现与分享的音乐产品，依托专业音乐人、DJ、好友推荐及社交功能，为用户打造全新的音乐生活。听到第一首就点了收藏歌单，下雨天晚上开着车在市区转转，应该很舒服！！',
+    headImgSrc: '/images/detail/carousel/02.jpg',
+    author: '钢铁侠 ',
+    dataTime: '24time',
+    detail_love_image1: '/images/icon/chat.png',
+    detail_love_image2: '/images/icon/view.png',
+    love_count: 188,
+    attention_count: 62,
+    detail: '钢铁战队。。',
+    music: {
+      dataUrl: 'http://up.mcyt.net/down/46100.mp3', // 音乐链接
+      title: 'IF-Ken Arai',   // 音乐标题
+      coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
+    },
+    postId: 0
+  },
+  {
+    
+    date: 'may 19 2018',
+    title: '点亮十七岁',
+    detail_img: '/images/detail/carousel/01.jpg',
+    avatar: '/images/avatar/4.png',
+    detail_content: '一 留声机黑胶拉你回到往昔，二 百老汇式舞台剧带你进入氛围，三 爵士蓝调让你在雨中舞动不休。很有韵味的歌单，同样很喜欢老电影，不过旧电影固然很经典，但是酒太醇的话，也会让人太沉醉而醒不过来 那种状态 真心不喜欢保持长久，希望你快乐，不会太沉重',
+    headImgSrc: '/images/detail/carousel/01.jpg',
+    author: '美国队长',
+    dataTime: '24time',
+    detail_love_image1: '/images/icon/chat.png',
+    detail_love_image2: '/images/icon/view.png',
+    love_count: 88,
+    attention_count: 66,
+    detail: '有魅力的老男人。',
+    music: {
+      dataUrl: 'http://www.ytmp3.cn/down/50395.mp3', // 音乐链接
+      title: '一路向北',   // 音乐标题
+      coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
+    },
+    postId: 1
+  },
+  {
+
+    date: 'may 19 2018',
+    title: '雨天的你专辑',
+    detail_img: '/images/detail/carousel/03.jpg',
+    avatar: '/images/avatar/4.png',
+    detail_content: '" 下雨了，睡下吧，听说，紧着雨，寒潮就到了，明日唤起你的可能就是跋涉千里而来的寒意。到时穿上大衣，哈着气出门，打着伞也挡不住随着风四处钻的雨雾；到时踩上今夜的积水，溅起的水珠子会坏坏地冰一下你的脚踝；到时约友人去奶茶店，点两杯热热的奶茶。睡下吧，为着今日瓢泼的雨和明日暖暖的茶。”',
+    headImgSrc: '/images/detail/carousel/03.jpg',
+    author: '绿巨人',
+    dataTime: '24time',
+    detail_love_image1: '/images/icon/chat.png',
+    detail_love_image2: '/images/icon/view.png',
+    love_count: 18,
+    attention_count: 56,
+    detail: '巨无霸教授。',
+    music: {
+      dataUrl: 'http://www.ytmp3.cn/down/50355.mp3', // 音乐链接
+      title: '听海',   // 音乐标题
+      coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
+    },
+    postId: 2
+  },
+  {
+    date: '2018/3/15 下午 4:30:35',
+    title: '潘玮柏全新打造',
+    detail_img: '/images/detail/list/j2.jpg',
+    detail_content: '潘玮柏和袁娅维的合作，我相信这首歌的人气，绝对不亚于欧美歌曲，吹爆！！！潘帅采用特色的硬核说唱，配上贯穿全篇歌词的嘻哈精神“love&respect，不服输”，让听众感受到拳拳到肉的精神力量。国际化的灵魂女声tia袁娅维也给这首歌增色不少，整首单曲旋律抓耳，以强有力的鼓点为编曲主色调，兼具力量与流行性。',
+    detail_love_image1: '/images/icon/chat.png',
+    detail_love_image2: '/images/icon/view.png',
+    love_count: 912,
+    headImgSrc: '/images/detail/list/j2.jpg',
+    author: '新华社',
+    attention_count: 188,
+    avatar: '/images/avatar/1.png',
+    music: {
+      dataUrl: 'http://up.mcyt.net/down/46101.mp3', // 音乐链接
+      title: 'Sunset Jesus-Avicii',   // 音乐标题
+      coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
+    },
+    postId: 3
+  },
+  {
+    date: '2018/3/17 下午3:30:35',
+    title: '尘土',
+    detail_img: '/images/detail/list/j3.jpg',
+    detail_content: '我从未想过我会高傲的存在，只想放低姿态 欣赏你 每天的自拍，灯光下你不能忽视 我的占据，每次散场 我 也 只能悄悄的离开，你伸出双手每次我都看得见，我过去热情围绕你却 视而不见，我才明白只是 透明存在，证明着陪伴才是我最深情告白，',
+    detail_love_image1: '/images/icon/chat.png',
+    detail_love_image2: '/images/icon/view.png',
+    love_count: 88,
+    attention_count: 66,
+    headImgSrc: '/images/detail/list/j3.jpg',
+    author: '新华社',
+    avatar: '/images/avatar/2.png',
+    music: {
+      dataUrl: 'http://up.mcyt.net/down/46102.mp3', // 音乐链接
+      title: '汪峰 - 儿时',   // 音乐标题
+      coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
+    },
+    postId: 4
+  },
+  {
+
+    date: 'sep 19 2016',
+    title: '网易生活',
+    detail_img: '/images/detail/list/j4.jpg',
+    avatar: '/images/avatar/3.png',
+    detail_content: 'switch godLe$ 刘思鉴 可以叫他蕾丝 丝丝或是刘爱军hhh 成都男娃娃 生日2月28日 可爱的双鱼座 小细腿 喜欢打球 爱睡懒觉 喜欢穿红裤衩 动不动两耳屎给起的傲娇小朋友 会放屁给粉丝听 性格很皮 很有才华 是个光头 爱把拖把堆在自己头上（嘘）时不时还卖会个萌 很棒的一个男生 喜欢就会一直喜欢ଲଇଉକ谢谢❤',
+    headImgSrc: '/images/detail/list/j4.jpg',
+    author: '李白3',
+    detail_love_image1: '/images/icon/chat.png',
+    detail_love_image2: '/images/icon/view.png',
+    love_count: 88,
+    attention_count: 66,
+    detail: '女神。。。',
+    music: {
+      dataUrl: 'http://up.mcyt.net/down/46110.mp3', // 音乐链接
+      title: '曲婉婷 - 我的歌声里-(电视剧《在线爱》主题曲)',   // 音乐标题
+      coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
+    },
+    postId: 5
+  },
+  {
+
+    date: 'sep 19 2016',
+    title: '海淘一下',
+    detail_img: '/images/detail/list/j6.jpg',
+    avatar: '/images/avatar/4.png',
+    detail_content: '暴躁老哥也会说“爱你们”然后装作自己被盗号删掉微博，你说全民那些粉丝不算什么，你说不想随便写点什么都被人瞎吹，没关系啊刘思鉴，你会成长，我也是。还记得16年你跟我说的“中考加油”，不知不觉，从五十个粉丝到现在，今天是喜欢你的第856天，我依然在。 ',
+    headImgSrc: '/images/detail/list/j6.jpg',
+    author: '新华社',
+    detail_love_image1: '/images/icon/chat.png',
+    detail_love_image2: '/images/icon/view.png',
+    love_count: 458,
+    attention_count: 226,
+    detail: '女神2。。。',
+    music: {
+      dataUrl: 'http://up.mcyt.net/down/46100.mp3', // 音乐链接
+      title: 'IF-Ken Arai',   // 音乐标题
+      coverImgUrl: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
+    },
+    postId: 6
+  },
+];
+
+module.exports = {list_data};
--- pages/list/list.js
+++ pages/list/list.js
@@ -1,18 +1,22 @@
 // pages/list/list.js
+//用相对路径
+let Datas = require('../../datas/list-data.js');
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+  listDatas:[],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+  this.setData({
+    listDatas: Datas.list_data,
+  })
   },
 
   /**
